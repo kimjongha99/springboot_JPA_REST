60ac68cd084bd2d48f88265e79b275ddb560d6ff
kimjongha99 <jjhsun99@gmail.com>
2023-08-07 15:57:21 +0900

    x

---
+++ b01/src/test/java/com/webex/b01/B01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webex.b01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
