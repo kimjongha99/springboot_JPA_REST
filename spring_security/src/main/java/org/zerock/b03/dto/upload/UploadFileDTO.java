2f098ad4f2ff57bda689832590cfd89f70a58914
kimjongha99 <jjhsun99@gmail.com>
2023-08-16 15:34:52 +0900

    게시판 다만들고 시큐리티적용

---
+++ spring_security/src/main/java/org/zerock/b03/dto/upload/UploadFileDTO.java
@@ -0,0 +1,14 @@
+package org.zerock.b03.dto.upload;
+
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+
+    private List<MultipartFile> files;
+}
+
