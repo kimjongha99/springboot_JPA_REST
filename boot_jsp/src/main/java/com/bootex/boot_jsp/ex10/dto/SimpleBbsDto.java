2bb49afea972ccda26bf073d6938b5c96aba4c71
kimjongha99 <jjhsun99@gmail.com>
2023-08-25 14:52:38 +0900

    .

--- boot_jsp/src/main/java/com/bootex/boot_jsp/ex10/dto/SimpleBbsDto.java
+++ boot_jsp/src/main/java/com/bootex/boot_jsp/ex10/dto/SimpleBbsDto.java
@@ -1,8 +1,11 @@
 package com.bootex.boot_jsp.ex10.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+
 public class SimpleBbsDto {
     private int id;
     private String writer;
