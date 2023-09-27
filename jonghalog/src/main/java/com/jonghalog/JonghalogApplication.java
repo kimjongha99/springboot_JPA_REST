6179c59502faaad2e055472b28af99f52106e4ed
kimjongha99 <jjhsun99@gmail.com>
2023-09-27 12:49:39 +0900

    호돌맨세팅

---
+++ jonghalog/src/main/java/com/jonghalog/JonghalogApplication.java
@@ -0,0 +1,13 @@
+package com.jonghalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonghalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonghalogApplication.class, args);
+    }
+
+}
---
+++ jonghalog/src/test/java/com/jonghalog/JonghalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonghalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonghalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
